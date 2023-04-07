665c2b2766f9278b441a4385366642aea6ec1654
michael silverio <michael.silverio@sun-asterisk.com>
2023-04-07 16:32:39 +0800

    Added PrismaService to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { JwtStrategy } from './strategy/jwt.strategy';
 import { AuthController } from './auth.controller';
 import { Auth0Module } from '../auth0/auth0.module';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../../src/prisma/prisma.service';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' }), Auth0Module],
-  providers: [JwtStrategy, AuthService],
+  providers: [JwtStrategy, AuthService, PrismaService],
   exports: [PassportModule],
   controllers: [AuthController],
 })
