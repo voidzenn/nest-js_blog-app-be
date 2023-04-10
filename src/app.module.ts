f4be15ea47b542e5fa41e6a9a3735fa788f83228
michael silverio <michael.silverio@sun-asterisk.com>
2023-04-10 11:41:47 +0800

    Removed app module unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { Auth0Module } from './auth0/auth0.module';
@@ -8,7 +6,5 @@ import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, UsersModule, Auth0Module, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
